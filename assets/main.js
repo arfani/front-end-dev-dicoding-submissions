13a1cc95d8372e033b7314555f2962380facb7a9
arfan <arfanihidayat@gmail.com>
2022-08-23 16:57:40 +0800

    point 1

---
+++ assets/main.js
@@ -0,0 +1,20 @@
+function dropDownMenu(){
+    const menu = document.querySelectorAll('nav ul li')
+    menu.forEach(i => {
+        i.classList.toggle('show-menu')
+    })
+}
+
+const btnIconMenu = document.querySelector('.icon-menu')
+btnIconMenu.addEventListener('click', dropDownMenu)
+
+const quotes = [
+    "Whether you think you can or you can't, you're right !",
+    "Just do it, don't think too much !!",
+    "Code is like humor. When you have to explain it, it’s bad",
+    "Simplicity is the soul of efficiency",
+    "Make it work, make it right, make it fast",
+]
+
+const quoteEl = document.querySelector('.quote q')
+quoteEl.innerText = quotes[Math.floor(Math.random() * quotes.length)]
